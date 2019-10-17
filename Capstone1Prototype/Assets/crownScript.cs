3cbc46a7bd33472d5695758f443ea0179786241e
Claudia Alvarez <caalva12@asu.edu>
2019-10-17 12:55:33 -0700

    Created panel

---
+++ Capstone1Prototype/Assets/crownScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crownScript : MonoBehaviour
+{
+    public GameObject body;
+    public GameObject infoScreen;
+    public Collider chakraCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+            Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (chakraCollider.Raycast(r, out hit, 1000.0f))
+            {
+                //AudioSource audioSource = GetComponent<AudioSource>();
+                //audioSource.Play();
+                infoScreen.SetActive(true);
+
+            }
+
+
+        }
+    }
+
+        public void CloseInfoScreen()
+        {
+            //AudioSource audioSource = GetComponent<AudioSource>();
+            //audioSource.Play();
+            infoScreen.SetActive(false);
+        }
+    }
